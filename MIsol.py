357b34285275bc318cd92fa9f3c9eadeec4e0d92
Anvar Mexmonov <anvarbekmehmonov@gmail.com>
2021-08-13 23:13:45 +0500

    Dastur yangilandi
    Dasturlar yangilanib boradi

--- MIsol.py
+++ MIsol.py
@@ -1,2 +1,21 @@
-x=float(input("Siz son kiriting:"))
-print(f"{x} ning kvadrati {x**2} ga teng")
+x=input("Parolni kiriting:")
+if str(x)=="Mexmonov":
+    print("Xush kelibsiz Anvar Mexmonov")
+    odamlar=[]
+    while True:
+        odam=input("Login kiriting:")
+        if odam in odamlar:
+            print("Ushbu login band, iltimos qaytadan urinib ko'ring!")
+        else:
+            odamlar.append(odam)
+            print(f"Xush kelibsiz {odam}")
+        if len(odamlar)==15:
+            break
+            print("Ro'yxat to'lganligi sababli ro'yxatga olish toxtatildi")
+else:
+    print("Siz Anvar Mexmonovmassiz, dastur siz uchun ishlamaydi")
+    
+        
+        
+    
+    
